d073cf2f7b18e2d6a13a3cb25937f39de555909a
Yassine1000L <yassneyas11@gmail.com>
2026-05-21 19:57:40 +0200

    refactor

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -2,22 +2,20 @@
 
 namespace App\Models;
 
+use Database\Factories\FaqFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Faq extends Model
 {
-    /** @use HasFactory<\Database\Factories\FaqFactory> */
+    /** @use HasFactory<FaqFactory> */
     use HasFactory;
 
-
     protected $fillable = [
-    'question',
-    'answer',
-    'category',
-    'user_id',
+        'question',
+        'answer',
+        'category',
+        'user_id',
 
     ];
-
-
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,15 +8,19 @@
 
     <div class="hero-content">
 
-        <h1>FC ERASMUS</h1>
+        <div style="background-color: rgba(0, 0, 0, 0.6); padding: 30px; border-radius: 15px; display: inline-block;">
+
+        <h1>FC ERASMUS !</h1>
 
         <p>
             Het laatste voetbalnieuws, transfers en updates.
         </p>
 
-        <a class="hero-btn" href="/news">
+        <a class="hero-btn" href="/news" style="margin-top: 10px; display: inline-block;">
             Bekijk Nieuws
         </a>
+
+        </div>
 
     </div>
 
