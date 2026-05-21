64b578d7be5fdb944e375a48ff536e9ac9c6f6da
Yassine1000L <yassneyas11@gmail.com>
2026-05-21 22:34:12 +0200

    route aangepast

--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,44 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NewsController;
+use App\Http\Controllers\AdminUserController;
 use App\Http\Controllers\FaqController;
-
-
-
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\Userzone\ProfileController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
 
 // de hoofdroute = ' / '
-Route::get('/', [App\Http\Controllers\WelcomeController::class, 'index'])->name('welcome');
-
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 
 /*
-rscr dient om niet telkens get te moeten schrijven bij elke route van news crud systeem, 
-auth middleware zorgt ervoor dat alleen ingelogde gebruikers toegang hebben tot deze routes 
+rscr dient om niet telkens get te moeten schrijven bij elke route van news crud systeem,
+auth middleware zorgt ervoor dat alleen ingelogde gebruikers toegang hebben tot deze routes
 */
 Route::resource('news', NewsController::class);
-
-
 
 // faq routes
 Route::resource('faq', FaqController::class);
 
-
-
-
-
-
-
-
-
-
-
-//----------------------------------------------------------------------------------------------------------
-
+// ----------------------------------------------------------------------------------------------------------
 
 // het zelf gemaakte routes
 Route::get('/dashboard', function () {
     return redirect('/');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+
+
+
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->prefix('admin')->group(function () {
+    Route::get('/users', [AdminUserController::class, 'index']);
+    Route::post('/users/{user}/toggle-admin', [AdminUserController::class, 'toggleAdmin']);
 });
 
 require __DIR__.'/auth.php';
