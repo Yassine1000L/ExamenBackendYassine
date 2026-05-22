3d00b1cd06660b374a8562721a2a2ea1428f940f
Yassine1000L <yassneyas11@gmail.com>
2026-05-22 21:38:21 +0200

    Voeg profielvelden toe: username, birthday, bio en profielfoto-upload

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -26,6 +26,10 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+            'username' => ['nullable', 'string', 'max:255'],
+            'birthday' => ['nullable', 'date'],
+            'bio' => ['nullable', 'string', 'max:1000'],
+            'profile_photo' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048'],
         ];
     }
 }
