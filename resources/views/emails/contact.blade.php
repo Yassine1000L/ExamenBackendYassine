ca56c92ce4a0f8f7a7010328dcdb33a2ed607dcf
Yassine1000L <yassneyas11@gmail.com>
2026-05-23 05:29:25 +0200

    Delete contact.blade.php

--- resources/views/emails/contact.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>Contact formulier</h1>
-
-<p><strong>Naam:</strong> {{ $data['naam'] }}</p>
-
-<p><strong>Email:</strong> {{ $data['email'] }}</p>
-
-<p><strong>Bericht:</strong></p>
-
-<p>{{ $data['bericht'] }}</p>
